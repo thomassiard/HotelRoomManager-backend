fa4df44d21897d8c413c25a9b43a32f721e4a452
Thomas Siard <75135103+thomassiard@users.noreply.github.com>
2023-08-19 22:49:23 +0200

    Register i Login radi, dodao nove collections u MongoDB

--- index.js
+++ index.js
@@ -15,18 +15,6 @@ app.get("/", async (req, res) => {
   res.json({ test: "radi" });
 });
 
-app.post("/asd", async (req, res) => {
-  let db = await connect("Test");
-  let inserted = await db.collection("Kolekcija").insertOne({
-    diga: "entao",
-  });
-  let item_id = inserted.insertedId;
-  let doc = await db.collection("Kolekcija").findOne({
-    _id: mongo.ObjectId(item_id),
-  });
-  res.json(doc);
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
--- routes/auth.js
+++ routes/auth.js
@@ -2,25 +2,46 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import connect from "../db.js"; // Ovdje importate ispravnu putanju do vašeg db.js
+import crypto from "crypto"; // Dodano za generiranje tajnog ključa
 
 const router = express.Router();
 
+// Generirajte tajni ključ
+const secretKey = crypto.randomBytes(64).toString("hex");
+
 router.post("/register", async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { fullName, email, phoneNumber, password, confirmPassword } =
+      req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    if (password != confirmPassword) {
+      return res.status(400).json({ message: "Passwords must match!" });
+    }
 
     const db = await connect("HRM");
     const usersCollection = db.collection("users");
 
+    const user = await usersCollection.findOne({ email });
+    if (user) {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+
     const newUser = {
-      username,
+      fullName,
+      email,
+      phoneNumber,
       password: hashedPassword,
     };
 
     await usersCollection.insertOne(newUser);
 
-    res.status(201).json({ message: "User registered successfully" });
+    // Generirajte JWT token za novoregistriranog korisnika
+    const token = jwt.sign({ userId: newUser._id }, secretKey, {
+      expiresIn: "12h",
+    });
+
+    res.status(201).json({ message: "User registered successfully", token });
   } catch (error) {
     console.error("Register error:", error);
     res.status(500).json({ message: "An error occurred" });
@@ -29,12 +50,12 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
 
-    const db = await connect("your-database-name");
+    const db = await connect("HRM");
     const usersCollection = db.collection("users");
 
-    const user = await usersCollection.findOne({ username });
+    const user = await usersCollection.findOne({ email });
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -44,8 +65,9 @@ router.post("/login", async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
-      expiresIn: "1h",
+    // Upotrijebite generirani tajni ključ za potpisivanje JWT tokena
+    const token = jwt.sign({ userId: user._id }, secretKey, {
+      expiresIn: "12h",
     });
 
     res.json({ token });
